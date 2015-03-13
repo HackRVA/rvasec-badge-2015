f028eeaee4b39a5e53ed4badec7673e83fa7a3df
badgesvn <badgesvn@089e1381-d811-4958-a57b-1c376018ee95>
2015-03-13 18:48:33 +0000

    updated initial commit and TODO
    git-svn-id: svn+ssh://www.killercats.com/rvasec-badge-2015-dev@39 089e1381-d811-4958-a57b-1c376018ee95

--- src/logos/hto4bit.c
+++ src/logos/hto4bit.c
@@ -2,17 +2,17 @@
 
 
 /*
- cc -o hto4bit hto4bit.c hackrva.h 
+ cc -o hto4bit hto4bit.c
 */
 
-
 /*
+
 #include "hackrva.h"
 
 #define CMAP() hackrva_data_cmap
 #define PIXEL() hackrva_data
 
-#define OUTNAME "hackoutrva"
+#define OUTNAME "hackrva"
 */
 
 #include "rvasec.h"
@@ -26,28 +26,28 @@ main() {
     int r, c;
 
 
-    fprintf(stdout, "static unsigned int width = %d;\n", width);
-    fprintf(stdout, "static unsigned int height = %d;\n", height);
+    fprintf(stdout, "const static unsigned int %s_width = %d;\n", OUTNAME, width);
+    fprintf(stdout, "const static unsigned int %s_height = %d;\n", OUTNAME, height);
 
     /* output header */
-    fprintf(stdout, "#define %s_data_cmap[16][3] = {\n", OUTNAME);
+    fprintf(stdout, "const static %s_data_cmap[16][3] = {\n", OUTNAME);
     for (r=0; r<16; r++) {
-        fprintf(stdout, "{ %d, %d, %d }\n", 
+        fprintf(stdout, "{ %d, %d, %d },\n", 
                 (unsigned char)CMAP()[r][0],
                 (unsigned char)CMAP()[r][1],
                 (unsigned char)CMAP()[r][3]);
 	};
-    fprintf(stdout, "}\n");
+    fprintf(stdout, "};\n");
 
-    fprintf(stdout, "static char %s_data[] = {\n", OUTNAME);
+    fprintf(stdout, "const static char %s_data[] = {\n", OUTNAME);
     for (r=0; r<height; r++) {
         for (c=0; c<width; c+=2) {
             //fprintf(stdout, "%d,", ((unsigned char)(PIXEL()[r*c]) >> 4) );
-            fprintf(stdout, "%02x,", 
+            fprintf(stdout, "0x%02x,", 
                 (unsigned char)(((PIXEL()[r*width+c]) << 4) | (unsigned char)(PIXEL()[r*width+c+1]) ));
         }
         fprintf(stdout, "\n");
     }
-    fprintf(stdout, "}\n");
+    fprintf(stdout, "};\n");
 
 }
