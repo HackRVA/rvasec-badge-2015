fb1f9158c309484bd7b8d7270c85f4168bf99bab
badgesvn <badgesvn@089e1381-d811-4958-a57b-1c376018ee95>
2015-03-27 23:54:13 +0000

    converted dos .h files back to unix  removed Hardware*.h unicode crap
    git-svn-id: svn+ssh://www.killercats.com/rvasec-badge-2015-dev@73 089e1381-d811-4958-a57b-1c376018ee95

--- src/HardwareProfile - PIC32MX460F512L PIM.h
+++ src/HardwareProfile - PIC32MX460F512L PIM.h
@@ -9,8 +9,8 @@
  Software License Agreement:
 
  The software supplied herewith by Microchip Technology Incorporated
- (the �Company�) for its PIC� Microcontroller is intended and
- supplied to you, the Company�s customer, for use solely and
+ (the Company) for its PIC Microcontroller is intended and
+ supplied to you, the Companys customer, for use solely and
  exclusively on Microchip PIC Microcontroller products. The
  software is owned by the Company and/or its supplier, and is
  protected under applicable copyright laws. All rights are reserved.
@@ -19,7 +19,7 @@
  civil liability for the breach of the terms and conditions of this
  license.
 
- THIS SOFTWARE IS PROVIDED IN AN �AS IS� CONDITION. NO WARRANTIES,
+ THIS SOFTWARE IS PROVIDED IN AN AS IS CONDITION. NO WARRANTIES,
  WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
  TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
  PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
